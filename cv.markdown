1b0a9eaafae8f7eb7c4b423c1e5060ff8d6cf7a8
mayconrcampos <maycon.campos@gmail.com>
2022-03-30 23:12:11 -0300

    Curriculum Vitae

---
+++ cv.markdown
@@ -0,0 +1,7 @@
+CV
+--
+
+
+A [Pen](https://codepen.io/sisball8/pen/dBxBGe) by [James Sison](https://codepen.io/sisball8) on [CodePen](https://codepen.io).
+
+[License](https://codepen.io/sisball8/pen/dBxBGe/license).
---
+++ script.js
@@ -0,0 +1,13 @@
+export default Vue.createApp({
+    data() {
+      return {
+        nome: 'Maycon R Campos',
+		idade: "39",
+		universidade: "Centro Universitário Cesumar - Unicesumar",
+        tecnologias: ["Python", "Linguagem C", "Linguagem C++", "Java", "PHP", "Javascript", "HTML", "CSS", "Bootstrap", "MySQL", "SQLite", "VueJS", "Paradigma Estruturado", "Programação Orientada a Objetos", "Estruturas de Dados", "API's Rest", "Versionamento Git", "Github", "continua..."],
+        atribuicoesEstagio: ["auxílio no atendimento de tickets junto ao sistema de chamados", "auxílio na orçamentação para composição de Termos de Referência", "auxílio nas atividades administrativas relativas à processos de compra, garantia e recebimento de mercadorias", "auxílio na manutenção em computadores (backup, formatação, instalação de softwares, limpeza externa/interna, troca de teclado, mouse, cabo de rede, cabo de energia, montagem e configuração de um segundo monitor, instalação de projetor e outros periféricos)", "auxílio no atendimento quanto ao uso das impressoras (atolamento de papel, troca de toner, orientação para impressão em formatos diferentes)",  "auxílio na conferência de estoque"]
+
+      }
+    },
+	
+  }).mount('#apepe')
