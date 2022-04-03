46488bda5bc6e25c412c79053fb6b0a52293de98
mayconrcampos <maycon.campos@gmail.com>
2022-04-03 18:57:32 -0300

    ajustes na div header onde tem a lista de links de atalho

--- script.js
+++ script.js
@@ -11,7 +11,8 @@ export default Vue.createApp({
 		    universidade: "Centro Universitário Cesumar - Unicesumar",
         tecnologias: ["Python", "Linguagem C", "Linguagem C++", "Java", "PHP", "Javascript", "HTML", "CSS", "Servidor Apache", "Bootstrap",  "MySQL", "SQLite", "VueJS", "Big Data", "Paradigma Estruturado", "Programação Orientada a Objetos", "Estruturas de Dados", "API's Rest", "Versionamento Git", "Git e Github", "Linux", "Diagramas UML", "Metodologia Scrum", "continua..."],
         atribuicoesEstagio: ["Auxílio no atendimento de tickets junto ao sistema de chamados", "Auxílio na orçamentação para composição de Termos de Referência", "Auxílio nas atividades administrativas relativas à processos de compra, garantia e recebimento de mercadorias", "Auxílio na manutenção em computadores (backup, formatação, instalação de softwares, limpeza externa/interna, troca de teclado, mouse, cabo de rede, cabo de energia, montagem e configuração de um segundo monitor, instalação de projetor e outros periféricos)", "Auxílio no atendimento quanto ao uso das impressoras (atolamento de papel, troca de toner, orientação para impressão em formatos diferentes)",  "Auxílio na conferência de estoque"],
-        fecha: false
+        fecha: false,
+        telaWidth: ""
 
       }
     },
@@ -46,20 +47,29 @@ export default Vue.createApp({
 
       formataData(data){
         var dataFormatada = new Date(data)
-        var dia = dataFormatada.getDay()
+        var dia = dataFormatada.getDate()
         var mes = dataFormatada.getMonth()
+      
         var ano = dataFormatada.getFullYear()
         var hora = dataFormatada.getHours()
         var minutos = dataFormatada.getMinutes()
         var segundos = dataFormatada.getSeconds()
 
         dia = dia < 10 ? "0"+dia : dia
-        mes = mes < 10 ? "0"+mes : mes
+        //mes = mes < 10 ? "0"+mes : mes
         hora = hora < 10 ? "0"+hora : hora 
         minutos = minutos < 10 ? "0"+minutos : minutos 
         segundos = segundos < 10 ? "0"+segundos : segundos
 
-        return `${dia}/${mes}/${ano} - ${hora}:${minutos}:${segundos}  `
+        const month = ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"];
+
+        return `${dia}/${month[mes]}/${ano} - ${hora}:${minutos}:${segundos}  `
+      },
+
+      getTotalWidth(){
+        this.telaWidth = window.innerWidth
+        
+        console.log(this.telaWidth)
       }
       
       
@@ -68,6 +78,9 @@ export default Vue.createApp({
       this.calculaIdade()
       this.getdadosAPIgithub()
       this.listaRepos()
-    }
+      window.addEventListener("resize", this.getTotalWidth);
+    },
+      
+ 
 	
   }).mount('#CV')
