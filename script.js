cc53e02cddbd330721a5aba17fd09ed6335f7064
mayconrcampos <maycon.campos@gmail.com>
2022-04-03 12:31:29 -0300

    inserindo dados extraidor do github app

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ export default Vue.createApp({
       return {
         nome: 'Maycon R. Campos',
 		    idade: "",
+        user: "mayconrcampos",
+        foto_avatar: "",
+        location: "",
 		    universidade: "Centro Universitário Cesumar - Unicesumar",
         tecnologias: ["Python", "Linguagem C", "Linguagem C++", "Java", "PHP", "Javascript", "HTML", "CSS", "Servidor Apache", "Bootstrap",  "MySQL", "SQLite", "VueJS", "Big Data", "Paradigma Estruturado", "Programação Orientada a Objetos", "Estruturas de Dados", "API's Rest", "Versionamento Git", "Git e Github", "Linux", "Diagramas UML", "Metodologia Scrum", "continua..."],
         atribuicoesEstagio: ["Auxílio no atendimento de tickets junto ao sistema de chamados", "Auxílio na orçamentação para composição de Termos de Referência", "Auxílio nas atividades administrativas relativas à processos de compra, garantia e recebimento de mercadorias", "Auxílio na manutenção em computadores (backup, formatação, instalação de softwares, limpeza externa/interna, troca de teclado, mouse, cabo de rede, cabo de energia, montagem e configuração de um segundo monitor, instalação de projetor e outros periféricos)", "Auxílio no atendimento quanto ao uso das impressoras (atolamento de papel, troca de toner, orientação para impressão em formatos diferentes)",  "Auxílio na conferência de estoque"],
@@ -17,11 +20,24 @@ export default Vue.createApp({
 
         this.idade = Math.floor(diferenca / (1000 * 60 * 60 * 24 * 365))
       },
+      async getdadosAPIgithub(){
+        await fetch(`https://api.github.com/users/${this.user}`)
+        .then(resposta => resposta.json())
+        .then(conteudo => {
+          this.foto_avatar = conteudo.avatar_url
+          this.location = conteudo.location
+          console.log(this.foto_avatar)
+
+
+          console.log(conteudo)
+        })
+      }
       
       
     },
     created(){
       this.calculaIdade()
+      this.getdadosAPIgithub()
     }
 	
   }).mount('#CV')
