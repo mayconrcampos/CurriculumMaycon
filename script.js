0aeaec34de3b3f0aeba1c5d5d35b34d4b5ff12a9
mayconrcampos <maycon.campos@gmail.com>
2022-04-03 12:38:10 -0300

    Inserindo dados vindos da api github

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 export default Vue.createApp({
     data() {
       return {
-        nome: 'Maycon R. Campos',
+        nome: '',
 		    idade: "",
         user: "mayconrcampos",
         foto_avatar: "",
         location: "",
+        bio: "",
+
 		    universidade: "Centro Universitário Cesumar - Unicesumar",
         tecnologias: ["Python", "Linguagem C", "Linguagem C++", "Java", "PHP", "Javascript", "HTML", "CSS", "Servidor Apache", "Bootstrap",  "MySQL", "SQLite", "VueJS", "Big Data", "Paradigma Estruturado", "Programação Orientada a Objetos", "Estruturas de Dados", "API's Rest", "Versionamento Git", "Git e Github", "Linux", "Diagramas UML", "Metodologia Scrum", "continua..."],
         atribuicoesEstagio: ["Auxílio no atendimento de tickets junto ao sistema de chamados", "Auxílio na orçamentação para composição de Termos de Referência", "Auxílio nas atividades administrativas relativas à processos de compra, garantia e recebimento de mercadorias", "Auxílio na manutenção em computadores (backup, formatação, instalação de softwares, limpeza externa/interna, troca de teclado, mouse, cabo de rede, cabo de energia, montagem e configuração de um segundo monitor, instalação de projetor e outros periféricos)", "Auxílio no atendimento quanto ao uso das impressoras (atolamento de papel, troca de toner, orientação para impressão em formatos diferentes)",  "Auxílio na conferência de estoque"],
@@ -26,7 +28,9 @@ export default Vue.createApp({
         .then(conteudo => {
           this.foto_avatar = conteudo.avatar_url
           this.location = conteudo.location
-          console.log(this.foto_avatar)
+          this.nome = conteudo.name
+          this.bio = conteudo.bio
+          
 
 
           console.log(conteudo)
